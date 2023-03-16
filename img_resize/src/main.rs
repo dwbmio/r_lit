cf7307271de19a0c18d629fa910775dfc8d49d25
dwb <dwb@dwb.ren>
2023-03-16 16:21:19 +0800

    fix: support window paht

--- img_resize/src/main.rs
+++ img_resize/src/main.rs
@@ -1,12 +1,9 @@
-use clap::{Parser, load_yaml, YamlLoader};
+use clap::{Parser, YamlLoader};
 use std::fs;
-use std::io::Error;
-use std::process::{exit, id};
+use std::process::{exit};
 use image::{GenericImageView, ImageFormat};
 use std::fs::File;
 use std::path::Path;
-use image::imageops::FilterType;
-use std::any::Any;
 
 #[derive(Parser)]
 #[clap(version = "1.0", author = "dwb<dwb@dwb.ren>")]
@@ -18,18 +15,13 @@ struct Opts {
     image: String,
 }
 
-
-const RESIZE_CONFIG: &str = "vec_size: [[10, 10], [10,20]]
-vec_f: [a.png, b.png]";
-
-
 fn main() {
     let opt = Opts::parse();
-    let cf = fs::read_to_string(opt.config).unwrap_or_else(|e| {
+    let cf = fs::read_to_string(opt.config).unwrap_or_else(|_| {
         println!("Load config failed!Create the config first");
         exit(2)
     });
-    let out = YamlLoader::load_from_str(cf.as_str()).unwrap_or_else(|e| {
+    let out = YamlLoader::load_from_str(cf.as_str()).unwrap_or_else(|_| {
         println!("Load config failed!Not in yaml fmt");
         exit(2)
     });
@@ -49,12 +41,17 @@ fn main() {
     let mut idx = 0;
     for o in o_s {
         let f = o_f.get(idx).unwrap().as_str().unwrap();
-        let f_p = &Path::new( base_f).join(f);
+        let f_p = &Path::new( base_f).join(f.replace("/", std::path::MAIN_SEPARATOR.to_string().as_str()));
+        if let Some(f_pp) = f_p.parent(){
+            if !f_pp.is_dir(){
+                let _ = fs::create_dir_all(f_pp);
+            }
+        }
         let fo = &mut File::create(f_p).unwrap();
         let fo_size = (o[0].as_i64().unwrap() as u32, o[1].as_i64().unwrap() as u32);
         println!("output file:{} <size->w={}, h={}>", f_p.as_path().to_str().unwrap(), fo_size.0, fo_size.1);
         let im_r = im.thumbnail(fo_size.0, fo_size.1);
-        im_r.write_to(fo, ImageFormat::Png);
+        let _= im_r.write_to(fo, ImageFormat::Png);
         idx += 1;
     }
 }
