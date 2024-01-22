37eeb62eafe51ec3b0a742557dffa13f949646c2
duanwenbin <duanwenbin@zuoyebang.com>
2024-01-22 21:08:39 +0800

    refine:重构clap 命令参数

--- img_resize/src/main.rs
+++ img_resize/src/main.rs
@@ -1,57 +1,137 @@
-use clap::{Parser, YamlLoader};
+use clap::{arg, command, value_parser, ArgAction, ArgGroup, Command};
+use re_tp::tp::{self, ReError, TpResize};
+
+use core::panic;
 use std::fs;
-use std::process::{exit};
-use image::{GenericImageView, ImageFormat};
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::process::exit;
+mod re_tp;
 
-#[derive(Parser)]
-#[clap(version = "1.0", author = "dwb<dwb@dwb.ren>")]
-struct Opts {
-    #[clap(short, long, default_value = "config.yaml")]
-    config: String,
-
-    #[clap(required = true)]
-    image: String,
-}
-
-fn main() {
-    let opt = Opts::parse();
-    let cf = fs::read_to_string(opt.config).unwrap_or_else(|_| {
-        println!("Load config failed!Create the config first");
-        exit(2)
-    });
-    let out = YamlLoader::load_from_str(cf.as_str()).unwrap_or_else(|_| {
-        println!("Load config failed!Not in yaml fmt");
-        exit(2)
-    });
-    let c = &out[0].to_owned();
-    let im = image::open(&opt.image).unwrap();
-    println!("load texture from {:}, dimensions={:?} color={:?}", &opt.image, im.dimensions(), im.color());
-
-    // The color method returns the image's ColorType
-    println!("{:?}", im.color());
-    let o_s = c["vec_size"].as_vec().unwrap();
-    let o_f = c["vec_f"].as_vec().unwrap();
-    let is = c["base_f"].is_badvalue();
-    let mut base_f = "";
-    if !is {
-        base_f = c["base_f"].as_str().unwrap();
-    }
-    let mut idx = 0;
-    for o in o_s {
-        let f = o_f.get(idx).unwrap().as_str().unwrap();
-        let f_p = &Path::new( base_f).join(f.replace("/", std::path::MAIN_SEPARATOR.to_string().as_str()));
-        if let Some(f_pp) = f_p.parent(){
-            if !f_pp.is_dir(){
-                let _ = fs::create_dir_all(f_pp);
-            }
-        }
-        let fo = &mut File::create(f_p).unwrap();
-        let fo_size = (o[0].as_i64().unwrap() as u32, o[1].as_i64().unwrap() as u32);
-        println!("output file:{} <size->w={}, h={}>", f_p.as_path().to_str().unwrap(), fo_size.0, fo_size.1);
-        let im_r = im.thumbnail(fo_size.0, fo_size.1);
-        let _= im_r.write_to(fo, ImageFormat::Png);
-        idx += 1;
+fn handle(pb: &str) {
+    let tar = Path::new(pb);
+    if tar.exists() {
+        panic!("path is not exists!")
     }
 }
+
+fn main() -> Result<(), ReError> {
+    let cli = command!() // requires `cargo` feature
+        .arg(
+            arg!(
+                -m --max_pixel <MAX_WIDTH> "Set the MAX-WIDTH to filter the textue."
+
+            )
+            // We don't have syntax yet for optional options, so manually calling `required`
+            .value_parser(value_parser!(u32)),
+        )
+        .arg(
+            arg!(
+                --rw <RESIZE_WIDTH> "Set the MAX-WIDTH to filter the textue."
+            )
+            // We don't have syntax yet for optional options, so manually calling `required`
+            .value_parser(value_parser!(u32))
+            .conflicts_with("max_pixel"),
+        )
+        .arg(
+            arg!(
+                --rh <RESIZE_HEIGHT> "Set the MAX-HEIGHT to filter the textue."
+            )
+            // We don't have syntax yet for optional options, so manually calling `required`
+            .value_parser(value_parser!(u32))
+            .conflicts_with("max_pixel"),
+        )
+        .arg(
+            arg!(
+                <path> "Single *.(png|jpg) path or just a path."
+            )
+            .required(true)
+            .value_parser(value_parser!(std::path::PathBuf)),
+        )
+        .group(
+            ArgGroup::new("filter")
+                .args(["rw", "rh"])
+                .multiple(true)
+                .requires_all(["rw", "rh"])
+                .conflicts_with("max_pixel"),
+        )
+        .get_matches();
+
+    let mut tp_handle = TpResize {
+        max_pixel: 0,
+        height: 0,
+        width: 0,
+        tp: PathBuf::new(),
+        out: None,
+    };
+    if let Some(mw) = cli.get_one::<u32>("max_pixel") {
+        tp_handle.max_pixel = *mw;
+    };
+
+    if let Some(tp) = cli.get_one::<PathBuf>("path") {
+        tp_handle.tp = tp.to_path_buf();
+    };
+
+    if let Some(mw) = cli.get_one::<u32>("rw") {
+        tp_handle.width = *mw;
+    };
+
+    if let Some(tp) = cli.get_one::<u32>("rh") {
+        tp_handle.height = *tp;
+    };
+
+    tp_handle.exec()?;
+    Ok(())
+}
+
+// fn main() {
+//     let opt = Opts::parse();
+//     let cf = fs::read_to_string(opt.config).unwrap_or_else(|_| {
+//         println!("Load config failed!Create the config first");
+//         exit(2)
+//     });
+//     let out = YamlLoader::load_from_str(cf.as_str()).unwrap_or_else(|_| {
+//         println!("Load config failed!Not in yaml fmt");
+//         exit(2)
+//     });
+//     let c = &out[0].to_owned();
+//     let im = image::open(&opt.image).unwrap();
+//     println!(
+//         "load texture from {:}, dimensions={:?} color={:?}",
+//         &opt.image,
+//         im.dimensions(),
+//         im.color()
+//     );
+
+//     // The color method returns the image's ColorType
+//     println!("{:?}", im.color());
+//     let o_s = c["vec_size"].as_vec().unwrap();
+//     let o_f = c["vec_f"].as_vec().unwrap();
+//     let is = c["base_f"].is_badvalue();
+//     let mut base_f = "";
+//     if !is {
+//         base_f = c["base_f"].as_str().unwrap();
+//     }
+//     let mut idx = 0;
+//     for o in o_s {
+//         let f = o_f.get(idx).unwrap().as_str().unwrap();
+//         let f_p =
+//             &Path::new(base_f).join(f.replace("/", std::path::MAIN_SEPARATOR.to_string().as_str()));
+//         if let Some(f_pp) = f_p.parent() {
+//             if !f_pp.is_dir() {
+//                 let _ = fs::create_dir_all(f_pp);
+//             }
+//         }
+//         let fo = &mut File::create(f_p).unwrap();
+//         let fo_size = (o[0].as_i64().unwrap() as u32, o[1].as_i64().unwrap() as u32);
+//         println!(
+//             "output file:{} <size->w={}, h={}>",
+//             f_p.as_path().to_str().unwrap(),
+//             fo_size.0,
+//             fo_size.1
+//         );
+//         let im_r = im.thumbnail(fo_size.0, fo_size.1);
+//         let _ = im_r.write_to(fo, ImageFormat::Png);
+//         idx += 1;
+//     }
+// }
---
+++ img_resize/src/re_tp.rs
@@ -0,0 +1,121 @@
+pub mod tp {
+    use image::{GenericImageView, ImageError, ImageFormat};
+    use rand::{distributions::Alphanumeric, thread_rng, Rng};
+    use std::{
+        ffi::OsString,
+        fs,
+        path::{Path, PathBuf},
+    };
+    use thiserror::Error;
+    use walkdir::WalkDir;
+
+    #[derive(Error, Debug)]
+    pub enum ReError {
+        #[error("{0}")]
+        IOError(#[from] std::io::Error),
+
+        #[error("{0}")]
+        ImageHandleError(#[from] ImageError),
+
+        #[error("{0}")]
+        WalkDirError(#[from] walkdir::Error),
+    }
+    pub struct TpResize {
+        pub(crate) max_pixel: u32,
+        pub(crate) width: u32,
+        pub(crate) height: u32,
+        pub(crate) tp: PathBuf,
+        pub(crate) out: Option<PathBuf>,
+    }
+
+    impl TpResize {
+        fn rand_filename() -> String {
+            let rand_string: String = thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(12)
+                .map(char::from)
+                .collect();
+
+            rand_string
+        }
+
+        fn re_tp(
+            tp: &PathBuf,
+            size: (u32, u32),
+            out: Option<PathBuf>,
+            is_thumb: bool,
+        ) -> Result<(), ReError> {
+            let out_i = out.unwrap_or(std::env::current_dir().expect("current dir get failed!"));
+            let im = image::open(tp).unwrap();
+            fs::create_dir_all(out_i)?;
+            println!(
+                "resize texture from {:?}, pixel={:?} fmt={:?} => size={:?}",
+                &tp,
+                im.dimensions(),
+                im.color(),
+                size
+            );
+            let ran_fname = OsString::from(Self::rand_filename().to_owned());
+            let f_name = tp.file_name().unwrap_or(&(ran_fname));
+            let fo = &mut std::fs::File::create(f_name).unwrap();
+
+            let im_r = match is_thumb {
+                true => im.thumbnail(size.0, size.1),
+                false => im.resize_exact(size.0, size.1, image::imageops::FilterType::CatmullRom),
+            };
+            let _ = im_r.write_to(fo, ImageFormat::Png)?;
+
+            Ok(())
+        }
+
+        fn sinle_tp(&self, path: &PathBuf, out: Option<PathBuf>) -> Result<(), ReError> {
+            let is_thumb = self.max_pixel > 0;
+            Self::re_tp(
+                path,
+                (
+                    if is_thumb { self.max_pixel } else { self.width },
+                    if is_thumb {
+                        self.max_pixel
+                    } else {
+                        self.height
+                    },
+                ),
+                out.clone(),
+                is_thumb,
+            )?;
+            Ok(())
+        }
+
+        fn walk(&self, path: &PathBuf, out: Option<PathBuf>) -> Result<(), ReError> {
+            let walker = WalkDir::new(path).into_iter();
+            println!("start walk dir :{}...", path.display());
+            for entry in walker.filter_entry(|e| !Self::is_hidden(e)) {
+                let entry = entry?;
+                if entry.path().is_file() {
+                    println!("walk to re_tp file :{:?}", entry.path().display());
+                    self.sinle_tp(&entry.path().to_path_buf(), out.clone())?
+                }
+            }
+            Ok(())
+        }
+
+        fn is_hidden(entry: &walkdir::DirEntry) -> bool {
+            entry
+                .file_name()
+                .to_str()
+                .map(|s| s.starts_with("."))
+                .unwrap_or(false)
+        }
+
+        pub fn exec(&self) -> Result<(), ReError> {
+            if !self.tp.exists() {
+                panic!("path not exists!");
+            }
+            println!("exec ...");
+            match self.tp.is_file() {
+                true => self.sinle_tp(&self.tp, self.out.to_owned()),
+                false => self.walk(&self.tp, self.out.to_owned()),
+            }
+        }
+    }
+}
